1383f36c9a56e61403afddfe93ad69c298e42b05
Daniel Pak <PakJuhnMan@gmail.com>
2017-01-13 11:06:08 -0700

    changed uptown exercise to streamline code and add different conditional functionality/ changed loops warmup

--- exercises/Uptown_function_you_up_exercise/uptown.js
+++ exercises/Uptown_function_you_up_exercise/uptown.js
@@ -8,37 +8,33 @@ var lyrics = ["This", "hit", "that", "ice", "cold",
               "with", "Saint", "Laurent", "Gotta", "kiss",
               "myself", "I'm", "so", "pretty"];
 
-/*var printOut = function (lyrics) {
-    var fullSong = lyrics.join(" ")
-    console.log(fullSong);
-}
 
-printOut(lyrics);
-
-var reverseAndPrint = function(lyrics) { 
+/*var reverseAndPrint = function(lyrics) { 
     var fullSongReversed = lyrics.reverse();
     var song = fullSongReversed.join(" ");
     console.log(song);
 }
 
-reverseAndPrint(lyrics);
+reverseAndPrint(lyrics);*/
 
-var everyOther = function(lyrics) { 
-    for(var i = 1; i < lyrics.length; i += 2) { 
+/*var everyOther = function(lyrics) { 
+    for(var i = 1; i < lyrics.length; i++) { 
         lyrics.splice(i, 1);
     }
     var other = lyrics.join(" ");
-    console.log(everyOther);
+    console.log(other);
 }
+
 everyOther(lyrics);*/
 
 var reverseEveryTwo = function (lyrics) { 
     var newString = "";
     for (var i = 1; i <= lyrics.length; i += 2){ 
         if (i < lyrics.length - 1) {
-        newString += lyrics[i] + " ";
-        newString += lyrics[i-1] + " ";
-            
+        newString += lyrics[i] + " " + lyrics[i-1] + " ";
+            if (lyrics.length % 2 === 0) { 
+                continue;
+            }
         } else if (i === lyrics.length) {
             newString += lyrics[i-1];
         }
@@ -47,3 +43,15 @@ var reverseEveryTwo = function (lyrics) {
 }
 
 reverseEveryTwo(lyrics);
+
+/*
+function flipLyrics(lyrics) {
+    var string = "";
+    for (var i = 0, j = 1; j < lyrics.length; i += 2, j += 2) {
+        string += lyrics[j] + " " + lyrics[i] + " ";
+    }
+    console.log(string);
+}
+
+flipLyrics(lyrics);*/
+//will only work with an even amount of elements;
---
+++ warmups/loops_warmup/loops.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < 101; i++) { 
+    if ((i % 2 === 0 ) && i > 0) { 
+    console.log("even");
+    }
+    else if ((i % 1 === 0) && i > 0){
+        console.log("odd")
+    } else {
+    console.log("zero");
+    }
+}
