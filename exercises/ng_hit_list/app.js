ed5f26e21f741cac1e91a46e48553403050615ba
Daniel Pak <PakJuhnMan@gmail.com>
2017-02-02 11:28:17 -0700

    Created a hit list exercise to practice with ng http requests and ng directives

---
+++ exercises/ng_hit_list/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("MyApplication", []);
+
+app.controller("MainController", ["$scope", "$http", function ($scope, $http) {
+
+
+    $scope.images = [];
+
+    $http.get("http://api.vschool.io:6543/hitlist.json")
+        .then(function (response) {
+            $scope.images.push((response.data));
+            console.log(response.data);
+        });
+    
+}]);
